ca70c35eef3e73cfcbfa26075b8290bf19b63c4b
Steve Guillaudeu <steveguil@gmail.com>
2024-02-11 09:16:27 -0500

    crawled Club Express for all text within .body-container

--- config.ts
+++ config.ts
@@ -1,9 +1,10 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
-  maxPagesToCrawl: 50,
+  url: "https://help.clubexpress.com/Content/Home.htm",
+  match: "https://help.clubexpress.com/Content/**",
+  selector: `.body-container`,
+  maxPagesToCrawl: 2000,
   outputFileName: "output.json",
-  maxTokens: 2000000,
+  maxTokens: 2000000
 };

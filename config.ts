b9d0c6f941c5bd49c00e8888c4707db307a4f02e
Steve Guillaudeu <steveguil@gmail.com>
2024-02-11 09:31:06 -0500

    Testing TCC crawl, worked

--- config.ts
+++ config.ts
@@ -1,10 +1,9 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://help.clubexpress.com/Content/Home.htm",
-  match: "https://help.clubexpress.com/Content/**",
-  selector: `.body-container`,
-  maxPagesToCrawl: 2000,
-  outputFileName: "output.json",
+  url: "https://www.tricitycyclists.org",
+  match: "https://www.tricitycyclists.org/**",
+  maxPagesToCrawl: 50,
+  outputFileName: "tcc-output.json",
   maxTokens: 2000000,
 };
